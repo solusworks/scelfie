0c1758921be12a57a6db63f772fb4bd0e6087106
Emma Hyams <ehyams@usc.edu>
2016-04-14 22:41:18 -0700

    FINALLY FIGURED OUT EDIT LAYOUT

--- src/frame/EditPanel.java
+++ src/frame/EditPanel.java
@@ -6,6 +6,9 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.image.BufferedImage;
 
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.ParallelGroup;
+import javax.swing.GroupLayout.SequentialGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -52,7 +55,112 @@ public class EditPanel extends JPanel {
 
 	private void createGUI()
 	{
-		JPanel backPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		/*JButton tl = new JButton("tl");
+		JButton tr = new JButton("tr");
+		JButton bl = new JButton("bl");
+		JButton br = new JButton("br");
+		
+		GroupLayout layout = new GroupLayout(this);
+		this.setLayout(layout);
+		
+		SequentialGroup horiz = layout.createSequentialGroup();
+		
+		ParallelGroup right = layout.createParallelGroup();
+		right.addComponent(tl);
+		right.addComponent(bl);
+		horiz.addGroup(right);
+		
+		ParallelGroup left = layout.createParallelGroup();
+		left.addComponent(tr);
+		left.addComponent(br);
+		horiz.addGroup(left);
+		
+		layout.setHorizontalGroup(horiz);
+		
+		SequentialGroup vert = layout.createSequentialGroup();
+		
+		ParallelGroup top = layout.createParallelGroup();
+		top.addComponent(tl);
+		top.addComponent(tr);
+		vert.addGroup(top);
+		
+		ParallelGroup bottom = layout.createParallelGroup();
+		bottom.addComponent(bl);
+		bottom.addComponent(br);
+		vert.addGroup(bottom);
+		
+		layout.setVerticalGroup(vert);*/
+		
+		//FILTER
+		JPanel filterPanel = new JPanel();
+		filterPanel.add(new JTextArea());
+		filterPanel.add(new JButton("Press this button!!!"));
+		filterPanel.setSize(new Dimension(150,390));
+		//PIC
+		BufferedImage croppedImage = image.getSubimage(200, 200, 400, 300);		
+		JLabel picLabel = new JLabel(new ImageIcon(croppedImage));
+		JPanel imagePanel = new JPanel();
+		imagePanel.setSize(new Dimension(300,400));
+		imagePanel.add(picLabel);
+		
+		GroupLayout layout = new GroupLayout(this);
+		this.setLayout(layout);
+		layout.setAutoCreateGaps(true);
+		layout.setAutoCreateContainerGaps(true);
+				
+		SequentialGroup horizontal = layout.createSequentialGroup();
+		
+		ParallelGroup backFilter = layout.createParallelGroup();
+		backFilter.addComponent(backButton);
+		backFilter.addComponent(filterPanel);
+		
+		horizontal.addGroup(backFilter);
+		
+		ParallelGroup imgBut = layout.createParallelGroup();
+		imgBut.addComponent(imagePanel);
+		imgBut.addComponent(saveButton);
+		horizontal.addGroup(imgBut);
+		
+		layout.setHorizontalGroup(horizontal);
+		
+		SequentialGroup vertical = layout.createSequentialGroup();
+		vertical.addComponent(backButton);
+		
+		ParallelGroup filterPic = layout.createParallelGroup();
+		filterPic.addComponent(filterPanel);
+		filterPic.addComponent(imagePanel);
+		vertical.addGroup(filterPic);
+		
+		vertical.addComponent(saveButton);
+		
+		layout.setVerticalGroup(vertical);
+		
+		/*ParallelGroup saveButtons = layout.createParallelGroup();
+		saveButtons.addComponent(saveButton);
+		saveButtons.addComponent(saveToMyScelfiesButton);
+		saveButtons.addComponent(uploadToCommunityButton);*/
+		
+		/*
+		SequentialGroup photoSave = layout.createSequentialGroup();
+		photoSave.addComponent(imagePanel);
+		photoSave.addGroup(saveButtons);
+		horizontal.addGroup(photoSave);
+		
+		layout.setHorizontalGroup(horizontal);
+		
+		SequentialGroup vertical = layout.createSequentialGroup();
+		vertical.addComponent(backButton);
+		
+		ParallelGroup filterPhoto = layout.createParallelGroup();
+		filterPhoto.addComponent(filterPanel);
+		filterPhoto.addComponent(imagePanel);
+		vertical.addGroup(filterPhoto);
+		
+		vertical.addGroup(saveButtons);
+		
+		layout.setVerticalGroup(vertical);*/
+		
+		/*JPanel backPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		backPanel.add(backButton);
 		
 		add(backPanel);
@@ -66,6 +174,7 @@ public class EditPanel extends JPanel {
 		
 		add(filterScroll);*/
 		
+		/*
 		JPanel filter = new JPanel();
 		filter.setBackground(new Color(100,20,255));
 		filter.setSize(new Dimension(150,390));
@@ -84,7 +193,7 @@ public class EditPanel extends JPanel {
 		imagePanel.add(picLabel);
 		
 		add(imagePanel);
-		add(savePanel);
+		add(savePanel);*/
 		
 		//(300,400)
 	}
