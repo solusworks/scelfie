8b78e96c53590cb72e159c80362c02e902302e93
Emma Hyams <ehyams@usc.edu>
2016-04-16 16:07:06 -0700

    Save implemented. Done added. Back functionality added.

--- src/frame/EditPanel.java
+++ src/frame/EditPanel.java
@@ -4,13 +4,23 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.ParallelGroup;
 import javax.swing.GroupLayout.SequentialGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -26,8 +36,11 @@ public class EditPanel extends JPanel {
 	private JButton saveButton;
 	private JButton saveToMyScelfiesButton;
 	private JButton uploadToCommunityButton;
+	private JButton doneButton;
 	
 	private JScrollPane filterScroll;
+	
+	private JFileChooser fileChooser;
 	
 	public EditPanel(ScelfieFrame inNav) {
 		// TODO Auto-generated constructor stub
@@ -44,52 +57,36 @@ public class EditPanel extends JPanel {
 		backButton = new JButton("Back");
 		saveButton = new JButton("Save");
 		saveToMyScelfiesButton = new JButton("Save to My SCelfies");
-		uploadToCommunityButton = new JButton("Upload to Community Album");
+		uploadToCommunityButton = new JButton("Share with Community");
 		filterScroll = new JScrollPane(new JTextArea());
+		doneButton = new JButton("Done");
+		
+		fileChooser = new JFileChooser();
 	}
 
 	private void addActionListeners()
 	{
-		
+		backButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				goBack();
+			}
+		});
+		saveButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				saveScelfie();
+			}
+		});
 	}
 
 	private void createGUI()
 	{
-		/*JButton tl = new JButton("tl");
-		JButton tr = new JButton("tr");
-		JButton bl = new JButton("bl");
-		JButton br = new JButton("br");
-		
-		GroupLayout layout = new GroupLayout(this);
-		this.setLayout(layout);
-		
-		SequentialGroup horiz = layout.createSequentialGroup();
-		
-		ParallelGroup right = layout.createParallelGroup();
-		right.addComponent(tl);
-		right.addComponent(bl);
-		horiz.addGroup(right);
-		
-		ParallelGroup left = layout.createParallelGroup();
-		left.addComponent(tr);
-		left.addComponent(br);
-		horiz.addGroup(left);
-		
-		layout.setHorizontalGroup(horiz);
-		
-		SequentialGroup vert = layout.createSequentialGroup();
-		
-		ParallelGroup top = layout.createParallelGroup();
-		top.addComponent(tl);
-		top.addComponent(tr);
-		vert.addGroup(top);
-		
-		ParallelGroup bottom = layout.createParallelGroup();
-		bottom.addComponent(bl);
-		bottom.addComponent(br);
-		vert.addGroup(bottom);
-		
-		layout.setVerticalGroup(vert);*/
+		//DONE
+		//JPanel doneButtonPan = new JPanel();
+		//doneButtonPan.add(doneButton, BorderLayout.EAST);
+		//doneButtonPan.add(Box.createGlue());
+		//doneButtonPan.add(Box.createGlue());
+		//doneButtonPan.add(Box.createGlue());
+		//doneButtonPan.add(doneButton, FlowLayout.RIGHT);
 		
 		//FILTER
 		JPanel filterPanel = new JPanel();
@@ -117,84 +114,91 @@ public class EditPanel extends JPanel {
 		horizontal.addGroup(backFilter);
 		
 		ParallelGroup imgBut = layout.createParallelGroup();
+		imgBut.addComponent(doneButton);
 		imgBut.addComponent(imagePanel);
-		imgBut.addComponent(saveButton);
+		
+		JPanel saveButtons = new JPanel();
+		//ParallelGroup saveButtons = layout.createParallelGroup();
+		saveButtons.add(saveButton);
+		saveButtons.add(saveToMyScelfiesButton);
+		saveButtons.add(uploadToCommunityButton);
+		
+		imgBut.addComponent(saveButtons);
 		horizontal.addGroup(imgBut);
 		
 		layout.setHorizontalGroup(horizontal);
 		
 		SequentialGroup vertical = layout.createSequentialGroup();
-		vertical.addComponent(backButton);
+		//vertical.addComponent(backButton);
+		
+		
+		ParallelGroup backDone = layout.createParallelGroup();
+		backDone.addComponent(backButton);
+		backDone.addComponent(doneButton, GroupLayout.Alignment.TRAILING);
+		vertical.addGroup(backDone);
 		
 		ParallelGroup filterPic = layout.createParallelGroup();
 		filterPic.addComponent(filterPanel);
 		filterPic.addComponent(imagePanel);
 		vertical.addGroup(filterPic);
 		
-		vertical.addComponent(saveButton);
+		vertical.addComponent(saveButtons);
 		
 		layout.setVerticalGroup(vertical);
-		
-		/*ParallelGroup saveButtons = layout.createParallelGroup();
-		saveButtons.addComponent(saveButton);
-		saveButtons.addComponent(saveToMyScelfiesButton);
-		saveButtons.addComponent(uploadToCommunityButton);*/
-		
-		/*
-		SequentialGroup photoSave = layout.createSequentialGroup();
-		photoSave.addComponent(imagePanel);
-		photoSave.addGroup(saveButtons);
-		horizontal.addGroup(photoSave);
-		
-		layout.setHorizontalGroup(horizontal);
-		
-		SequentialGroup vertical = layout.createSequentialGroup();
-		vertical.addComponent(backButton);
-		
-		ParallelGroup filterPhoto = layout.createParallelGroup();
-		filterPhoto.addComponent(filterPanel);
-		filterPhoto.addComponent(imagePanel);
-		vertical.addGroup(filterPhoto);
-		
-		vertical.addGroup(saveButtons);
-		
-		layout.setVerticalGroup(vertical);*/
-		
-		/*JPanel backPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		backPanel.add(backButton);
-		
-		add(backPanel);
-		
-		/*
-		JPanel filterPanel = new JPanel();
-		filterPanel.add(new JTextArea());
-		filterPanel.add(new JButton("Press this button!!!"));
-		filterScroll.setSize(new Dimension(150,390));
-		filterScroll.add(filterPanel);
-		
-		add(filterScroll);*/
-		
-		/*
-		JPanel filter = new JPanel();
-		filter.setBackground(new Color(100,20,255));
-		filter.setSize(new Dimension(150,390));
-		add(filter);
-		
-		JPanel savePanel = new JPanel();
-		savePanel.add(saveButton);
-		savePanel.add(saveToMyScelfiesButton);
-		savePanel.add(uploadToCommunityButton);
-
-		BufferedImage croppedImage = image.getSubimage(200, 200, 400, 300);
-		
-		JLabel picLabel = new JLabel(new ImageIcon(croppedImage));
-		JPanel imagePanel = new JPanel();
-		imagePanel.setSize(new Dimension(300,400));
-		imagePanel.add(picLabel);
-		
-		add(imagePanel);
-		add(savePanel);*/
-		
-		//(300,400)
 	}
+	
+	private void goBack()
+	{
+		navigator.setImage(null);
+		navigator.setImageFile(null);
+		navigator.toHome();
+	}
+	
+	private void saveScelfie()
+	{
+		fileChooser.setDialogTitle("Save as...");
+		int returnVal = fileChooser.showSaveDialog(null);
+		
+		//BufferedWriter output = null;
+		
+		if(returnVal == JFileChooser.APPROVE_OPTION)
+		{
+			//File file = fileChooser.getSelectedFile();
+			//String path = file.getAbsolutePath();
+			try {
+				ImageIO.write(navigator.getImage(), "png", fileChooser.getSelectedFile());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+		/*c.setDialogTitle("Save as...");
+			int returnVal = fc.showSaveDialog(null);
+			
+			if(returnVal == JFileChooser.APPROVE_OPTION)
+			{
+				try {
+					file_selected = fc.getSelectedFile();								
+					
+					//String fname = "";
+					
+					if(file_selected.getName().endsWith(".txt"))
+					{
+						output = new BufferedWriter(new FileWriter(file_selected));
+						tabs.setTitleAt(tabs.getSelectedIndex(), file_selected.getName());
+						//fname = file_selected.getName();
+					}
+					else
+					{			
+						output = new BufferedWriter(new FileWriter(file_selected+".txt"));
+						tabs.setTitleAt(tabs.getSelectedIndex(), file_selected.getName()+".txt");
+						//fname = file_selected.getName() + ".txt"
+					}
+
+ 				output.write(file_contents);
+ 				output.flush();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} */
 }
