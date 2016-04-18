cbda4a62ecc5604950bd54ebce97a9c0b3f33c8d
Wai Kin, Keith Hon <waikinho@usc.edu>
2016-04-17 21:14:33 -0700

    Push again for the previous commit

--- src/client/ScelfieManager.java
+++ src/client/ScelfieManager.java
@@ -55,7 +55,7 @@ public class ScelfieManager {
 	}
 
 	public void addStickerToImg(String filePath, String feature) {
-		
+		 
 		Rect[] targetRect = null;
 		editedImg = cloneBufferedImageFrom(rawImg);
 		
