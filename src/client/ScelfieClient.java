d496ca72cbd9db06153710ee3d8a2937c9e1c4d6
Wai Kin, Keith Hon <waikinho@usc.edu>
2016-04-17 19:40:17 -0700

    no message

---
+++ src/client/ScelfieClient.java
@@ -0,0 +1,48 @@
+package client;
+
+import javax.swing.JFrame;
+
+public class ScelfieClient extends JFrame {
+
+	private ScelfieNetworkManager networkManager;
+	
+	ScelfieClient() {
+
+		super();
+		initializeVariables();
+		createGUI();
+		addActionAdapters();
+		setSize(800, 600);
+		setVisible(true);
+		
+	}
+	//Emma testing source tree
+	
+	void initializeVariables(){
+		
+		networkManager = new ScelfieNetworkManager();
+		networkManager.uploadImageMultipart("testImage.png");
+		
+		//No parameter for this POST method call
+		//System.out.println(ScelfieNetworkManager.excutePost("http://www.non-solus.com/scelfie/upload.php", ""));
+		//System.out.println(ScelfieNetworkManager.uploadImage("testImage.png"));
+	}
+
+	void createGUI(){
+		
+		
+	}
+	
+	void addActionAdapters(){
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+
+		new ScelfieClient();
+		
+	}
+
+}
